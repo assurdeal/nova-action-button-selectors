9f9bf78800e17f2d26077d38f1280e38f258b977
Percy Mamedy <percymamedy@gmail.com>
2024-06-20 12:20:24 +0400

    feat: Diverge package to assurdeal vendor

--- src/AssetServiceProvider.php
+++ src/AssetServiceProvider.php
@@ -1,6 +1,8 @@
 <?php
 
-namespace Lednerb\ActionButtonSelector;
+declare(strict_types=1);
+
+namespace Assurdeal\ActionButtonSelector;
 
 use Illuminate\Support\ServiceProvider;
 use Laravel\Nova\Events\ServingNova;
@@ -10,24 +12,12 @@ class AssetServiceProvider extends ServiceProvider
 {
     /**
      * Bootstrap any application services.
-     *
-     * @return void
      */
-    public function boot()
+    public function boot(): void
     {
         Nova::serving(function (ServingNova $event) {
             Nova::script('action-button-selector', __DIR__ . '/../dist/js/asset.js');
             Nova::style('action-button-selector', __DIR__ . '/../dist/css/asset.css');
         });
-    }
-
-    /**
-     * Register any application services.
-     *
-     * @return void
-     */
-    public function register()
-    {
-        //
     }
 }
